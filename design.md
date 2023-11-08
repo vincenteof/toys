0a8d9130f4266398617fd42e77463d92ca30d1aa
vincenteof <14847208+vincenteof@users.noreply.github.com>
2023-11-08 22:06:07 +0800

    feat: object store

--- design.md
+++ design.md
@@ -1,3 +1,17 @@
+# how it works
+blob 是存储的是文件内容
+tree 存储的是当前目录结构的 snapshot，它包含 blob
+一旦有了对应的 SHA-1，我们总能拿到对应的内容
+commit 则是 tree (snapshot) 和一些附加信息，比如 message 的集合
+commit 是有时间线的，因此生存 commit 的时候需要有上一个 commit 的 SHA-1
+因此只要能拿到最新一个 commit 的 SHA，我们自然也能知道它的所有历史，只要不断往上找 parent 即可
+blob，tree 以及 commit 会形成一个 DAG
+
+
 # 参考
 https://zhuanlan.zhihu.com/p/93950349
+https://git-scm.com/book/en/v2/Git-Internals-Git-Objects
+https://git-scm.com/book/en/v2/Git-Internals-Git-References
+https://git-scm.com/book/en/v2/Git-Internals-Packfiles
 https://youtu.be/4XpnKHJAok8
+https://youtu.be/bSA91XTzeuA
--- src/ToyGit/ObjectStore.hs
+++ src/ToyGit/ObjectStore.hs
@@ -1,10 +1,41 @@
 module ToyGit.ObjectStore where
-import ToyGit.Core (Hash, Content)
+
+import qualified Codec.Compression.Zlib as Zlib
+import Crypto.Hash (hash)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
+import System.Directory (createDirectoryIfMissing, doesFileExist)
+import System.FilePath (takeDirectory, (</>))
+import ToyGit.Core (Content, Hash)
 
+-- Follw what git really does, just replace `git` with `toygit`
+objectStorePath :: String
+objectStorePath = ".toygit/objects"
 
+-- Compute the path of an object
+-- `git` uses first two characters of the hash as the subfolder and the rest of the hash as filename
+objectPath :: Hash -> FilePath
+objectPath hash = objectStorePath </> take 2 hashString </> drop 2 hashString
+  where
+    hashString = show hash
+
+-- `ObjectStore` acts like an kv database
+-- The key is the hash of the object, and the value is the zlib-deflated content
 storeObject :: Hash -> Content -> IO ()
-storeObject hash content = undefined
+storeObject hash content = do
+  let path = objectPath hash
+      compressed = Zlib.compress $ LBS.fromStrict content
+  createDirectoryIfMissing True (takeDirectory path)
+  LBS.writeFile path compressed
 
-retrieveObject :: Hash -> IO Content
-retrieveObject hash = undefined
+-- The retrieve process is just the opposite of the store process
+retrieveObject :: Hash -> IO (Maybe Content)
+retrieveObject hash = do
+  let path = objectPath hash
+  exists <- doesFileExist path
+  if exists
+    then do
+      compressedContent <- LBS.readFile path
+      let content = Zlib.decompress compressedContent
+      return $ Just . LBS.toStrict $ content
+    else return Nothing

69f35521129e0b01f9361d3d34fcde0b8a1f118a
vincenteof <14847208+vincenteof@users.noreply.github.com>
2023-11-11 23:42:43 +0800

    feat: blob

--- src/ToyGit/Blob.hs
+++ src/ToyGit/Blob.hs
@@ -1,12 +1,21 @@
 module ToyGit.Blob where
 
+import ToyGit.Core (Hash, hashContent)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
 
--- data Blob = Blob Content
-newtype Blob = Blob BS.ByteString
+newtype Blob = Blob {unBlob :: BS.ByteString}
 
 serializeBlob :: Blob -> BS.ByteString
-serializeBlob (Blob content) = content
+serializeBlob (Blob content) = header <> content
+  where
+    header = BC.pack $ "blob " ++ show (BS.length content) ++ "\0"
 
-createBlob :: FilePath -> IO Blob
-createBlob path = Blob <$> BS.readFile path
+deserializeBlob :: BS.ByteString -> Blob
+deserializeBlob bs = Blob $ BS.drop (headerLength bs) bs
+
+headerLength :: BS.ByteString -> Int
+headerLength bs = maybe 0 (+ 1) (BS.elemIndex 0 bs)
+
+hashBlob :: Blob -> Hash
+hashBlob = hashContent . serializeBlob

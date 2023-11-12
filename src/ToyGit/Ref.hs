ef451f7976ef4bb25ed1cd1fc394503cab30c264
vincenteof <14847208+vincenteof@users.noreply.github.com>
2023-11-12 21:34:38 +0800

    feat: add ref

--- src/ToyGit/Ref.hs
+++ src/ToyGit/Ref.hs
@@ -1,16 +1,24 @@
 module ToyGit.Ref where
 
-import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BC
+import System.Directory (createDirectoryIfMissing, doesFileExist)
 import System.FilePath ((</>))
+import System.FilePath.Posix (takeDirectory)
 import ToyGit.Core (Hash, hashContent)
-import ToyGit.Util (hashToHex)
 
-updateRef :: String -> ByteString -> IO ()
-updateRef refName content = BC.writeFile (refPath refName) (hashToHex $ hashContent content)
+writeRef :: FilePath -> String -> Hash -> IO ()
+writeRef repoPath refName hash = do
+  let refPath = getRefPath repoPath refName
+  createDirectoryIfMissing True (takeDirectory refPath)
+  BC.writeFile refPath (BC.pack $ show hash)
 
-getRef :: String -> IO (Maybe Hash)
-getRef refName = undefined
+readRef :: FilePath -> String -> IO (Maybe Hash)
+readRef repoPath refName = do
+  let refPath = getRefPath repoPath refName
+  exists <- doesFileExist refPath
+  if exists
+    then Just . read . BC.unpack <$> BC.readFile refPath
+    else return Nothing
 
-refPath :: String -> FilePath
-refPath refName = "toygit-refs" </> refName
+getRefPath :: FilePath -> String -> FilePath
+getRefPath repoPath refName = repoPath <> ".toygit" </> "refs" </> "heads" </> refName

96988ba85a8d61edf26b8e0e734b3869cbc15c5e
vincenteof <14847208+vincenteof@users.noreply.github.com>
2023-11-12 16:37:23 +0800

    feat: commit

--- src/ToyGit/Commit.hs
+++ src/ToyGit/Commit.hs
@@ -1,17 +1,36 @@
 module ToyGit.Commit where
 
 import Data.ByteString (ByteString)
-import ToyGit.Core (Hash)
+import qualified Data.ByteString.Char8 as BC
+import Data.Time.Clock (UTCTime)
+import Data.Time.Format (defaultTimeLocale, formatTime)
+import ToyGit.Core (Hash, hashContent)
 import ToyGit.Tree (Tree)
 
 data Commit = Commit
   { commitTree :: Hash,
-    commitParent :: Maybe Hash,
-    commitMessage :: String
+    commitParents :: [Hash],
+    commitAuthor :: String,
+    commitCommitter :: String,
+    commitMessage :: String,
+    commitTime :: UTCTime
   }
+  deriving (Show, Eq)
 
 serializeCommit :: Commit -> ByteString
-serializeCommit = undefined
+serializeCommit commit =
+  BC.pack $
+    unlines
+      [ "tree " ++ show (commitTree commit),
+        unlines $ map (("parent " ++) . show) (commitParents commit),
+        "author " ++ commitAuthor commit,
+        "committer " ++ commitCommitter commit,
+        "",
+        commitMessage commit
+      ]
+  where
+    formatTimeStr = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%SZ"
+    formattedTime = formatTimeStr (commitTime commit)
 
-createCommit :: Tree -> Maybe Hash -> String -> IO Commit
-createCommit = undefined
+hashCommit :: Commit -> Hash
+hashCommit = hashContent . serializeCommit

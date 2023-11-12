09554bff1727e7b67e57e7368ff57da09b5a2894
vincenteof <14847208+vincenteof@users.noreply.github.com>
2023-11-12 16:17:14 +0800

    feat: tree

--- src/ToyGit/Tree.hs
+++ src/ToyGit/Tree.hs
@@ -1,16 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ToyGit.Tree where
 
-import Data.ByteString (ByteString)
-import ToyGit.Core (Hash)    
+import qualified Data.ByteArray.Encoding as BA (Base (Base16), convertToBase)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.Map as Map
 import ToyGit.Blob (Blob)
+import ToyGit.Core (Hash, hashContent)
 
+data TreeEntry
+  = TreeFile FilePath Hash
+  | TreeDirectory FilePath Tree
+  deriving (Show, Eq)
 
-data TreeEntry = TreeFile Hash | TreeDirectory Tree
--- data Tree = Tree [(FilePath, TreeEntry)]
-newtype Tree = Tree [(FilePath, TreeEntry)]
+newtype Tree = Tree {unTree :: Map.Map FilePath TreeEntry}
+  deriving (Show, Eq)
 
-serializeTree :: Tree -> ByteString
-serializeTree = undefined
+serializeTree :: Tree -> BS.ByteString
+serializeTree (Tree entries) = BS.concat . Map.elems $ Map.mapWithKey serializeEntry entries
 
-createTree :: [(FilePath, Blob)] -> IO Tree
-createTree files = undefined
+serializeEntry :: FilePath -> TreeEntry -> BS.ByteString
+serializeEntry path (TreeFile _ hash) = BS.concat ["100644 blob ", hashToDex hash, "\0", BC.pack path]
+  where
+    hashToDex = BA.convertToBase BA.Base16
+serializeEntry path (TreeDirectory _ tree) = serializeTree tree
+
+hashTree :: Tree -> Hash
+hashTree = hashContent . serializeTree
